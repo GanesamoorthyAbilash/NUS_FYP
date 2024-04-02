1403a5bf13fb382bc753cbe3f40a4d60d4ca78c3
Abilash Ganesamoorthy <43057892+GanesamoorthyAbilash@users.noreply.github.com>
2024-04-03 00:20:37 +0800

    Add files via upload

---
+++ Pinpong_sample_matchfilter/app_peripherals.h
@@ -0,0 +1,30 @@
+/***************************************************************************//**
+ * @file
+ * @brief iostream usart examples functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#ifndef APP_PERIPHERALS_H
+#define APP_PERIPHERALS_H
+
+#include <stdint.h>
+
+void IADCRescale(uint32_t newScale);
+void initGPIO(void);
+void initIADC(void);
+void initLDMA(uint16_t *buffer, uint32_t size);
+void app_iostream_usart_init(void);
+void calc_action(uint16_t *buffer);
+
+#endif
